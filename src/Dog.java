15d031ebcc780bf3d8a3a3053323e42df086c3d1
jgaurav <Gaurav2.Jaiswal@ril.com>
2020-07-19 23:47:13 +0530

    Added abc and some changes in dog class

--- src/Dog.java
+++ src/Dog.java
@@ -2,5 +2,7 @@ public class Dog {
     public static void main(String[] args) {
         System.out.println("Hello_world -> from java language : ");
         System.out.println("Hello_gaurav -> from the programmer desk: ");
+        System.out.println("Hello everyone:");
+        System.out.println("hello python:");
     }
 }

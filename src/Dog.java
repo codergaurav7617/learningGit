51828d6d405573ee2aa71ea943504588314132e5
jgaurav <Gaurav2.Jaiswal@ril.com>
2020-07-19 23:49:56 +0530

    Added def and some code changes in dog class

--- src/Dog.java
+++ src/Dog.java
@@ -1,6 +1,5 @@
 public class Dog {
     public static void main(String[] args) {
         System.out.println("Hello_world -> from java language : ");
-        System.out.println("Hello_gaurav -> from the programmer desk: ");
     }
 }

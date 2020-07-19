0f44a3e25b936dcd3fb371a68ebcb2b442eae51a
jgaurav <Gaurav2.Jaiswal@ril.com>
2020-07-19 23:43:56 +0530

    Added Dog class

---
+++ src/Dog.java
@@ -0,0 +1,6 @@
+public class Dog {
+    public static void main(String[] args) {
+        System.out.println("Hello_world -> from java language : ");
+        System.out.println("Hello_gaurav -> from the programmer desk: ");
+    }
+}
